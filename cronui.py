f91920c588291a64ec87380bc35632e183d7b017
Elkin Aguas <elkinaguas@gmail.com>
2023-01-25 00:36:36 +0100

    Import cronmod packet

--- cronui.py
+++ cronui.py
@@ -1,10 +1,16 @@
 from textual.app import App, ComposeResult
 from textual.containers import Container
 from textual.widgets import Button, Header, Footer, Static, ListView, ListItem, Label
+from tools.cronmod import get_cron_jobs
+
 
 class crons(Static):
+    def __init__(self):
+        self.crons = get_cron_jobs()
+        super().__init__()
+
     def compose(self) -> ComposeResult:
-        options = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
+        options = [f"{self.crons[cron]['next_exec']} | {self.crons[cron]['command']}" for cron in self.crons]
         arguments = [ListItem(Label(option)) for option in options]
 
         yield ListView(
